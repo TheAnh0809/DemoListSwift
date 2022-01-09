42f3ca72ef0333b959fb47df4c121c1af0eed9d3
Shi Ying Nguyen <>
2022-01-09 11:33:29 +0700

    test commit

--- Demo_ListView/Demo_ListView/Controller/Demo_FirstTabbar.swift
+++ Demo_ListView/Demo_ListView/Controller/Demo_FirstTabbar.swift
@@ -10,12 +10,13 @@ class Demo_FirstTabbar : UIViewController{
     @IBOutlet weak var Demo_Segmen: UISegmentedControl!
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("cuong test")
     }
     @IBAction func changeSegmen(_ sender: Any) {
         switch Demo_Segmen.selectedSegmentIndex{
         case 0 :
             view.backgroundColor = .red
-        default:  
+        default:
             view.backgroundColor = .green
         }
     }
